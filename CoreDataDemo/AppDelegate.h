214ae27d184419d6b0bd3f28352b32951b18f556
mac <mac@macs-Mac-mini.local>
2014-05-07 14:36:28 +0530

    Initial Commit

---
+++ CoreDataDemo/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  CoreDataDemo
+//
+//  Created by mac on 07/05/14.
+//  Copyright (c) 2014 Webonise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
